34b10505c0fb7a15bad083547b698abc7c879801
Venuja Vethmin <venujavethmin@gmail.com>
2025-07-06 21:29:41 +0530

    .

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import authMiddleware from '../middleware/authMiddleware.js';
-import { login, protectedRoute, register } from "../controllers/auth.controller.js";
+import { getAllUsers, login, protectedRoute, register } from "../controllers/auth.controller.js";
 
 
 const authRoute = express.Router();
 
+
+authRoute.get("/getUsers", getAllUsers);
 authRoute.post("/register", register);
 authRoute.post("/login", login);
 authRoute.get("/protected", authMiddleware, protectedRoute);
